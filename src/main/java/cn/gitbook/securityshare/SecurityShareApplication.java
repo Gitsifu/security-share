da9419793ef6a21dffccaaed8fe719a8ab0277cf
SmartGim <419771681@qq.com>
2018-08-05 22:47:58 +0800

    init commit

---
+++ src/main/java/cn/gitbook/securityshare/SecurityShareApplication.java
@@ -0,0 +1,12 @@
+package cn.gitbook.securityshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityShareApplication.class, args);
+	}
+}
